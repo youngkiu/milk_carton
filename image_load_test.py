cb8cde3fbdb1ca40d6336d8c10b2071d2193921f
youngki lyu <youngkiu@gmail.com>
2017-04-28 15:00:10 +0900

    implement the random selection of the training data file

--- image_load_test.py
+++ image_load_test.py
@@ -1,6 +1,7 @@
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 from PIL import Image
 
@@ -33,19 +34,38 @@ def loadImage(imageFile, labelFile):
 
     return xy
 
-
-if __name__ == '__main__':
+def selectTrainingData():
     normal_img_dir = "skive/normal_img/"
     defect_img_dir = "skive/defect_img/"
 
-    files = os.listdir(normal_img_dir)
-    for file in files:
-        if file.find("label") < 0:
-            xy = loadImage("{0}{1}".format(normal_img_dir, file), \
-                           "{0}{1}".format(normal_img_dir, "label.png"))
+    numOfNormalImg = len(os.listdir(normal_img_dir)) - 1 # All image share a label
+    numOfDefectImg = len(os.listdir(defect_img_dir)) / 2 # Each image has a label
+    numOfTrainingData = numOfNormalImg + numOfDefectImg
 
-    files = os.listdir(defect_img_dir)
-    for file in files:
-        if file.find("image") >= 0:
-            xy = loadImage("{0}{1}".format(defect_img_dir, file), \
-                           "{0}{1}".format(defect_img_dir, file.replace("image", "label")))
+    selectedIndex = random.randint(0, numOfTrainingData - 1)
+    #print(selectedIndex, numOfTrainingData)
+
+    if selectedIndex < numOfNormalImg:
+        fileIndex = 0
+        files = os.listdir(normal_img_dir)
+        for file in files:
+            if file.find("label") < 0:
+                fileIndex += 1
+                if fileIndex == selectedIndex:
+                    xy = loadImage("{0}{1}".format(normal_img_dir, file), \
+                                   "{0}{1}".format(normal_img_dir, "label.png"))
+    else:
+        fileIndex = numOfNormalImg
+        files = os.listdir(defect_img_dir)
+        for file in files:
+            if file.find("image") >= 0:
+                fileIndex += 1
+                if fileIndex == selectedIndex:
+                    xy = loadImage("{0}{1}".format(defect_img_dir, file), \
+                                   "{0}{1}".format(defect_img_dir, file.replace("image", "label")))
+
+if __name__ == '__main__':
+    random.seed(20170428)
+
+    for i in range(1000):
+        xy = selectTrainingData()
