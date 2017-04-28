3108a48a3316e0907ccfe4d6db10234dd3fa5001
youngki lyu <youngkiu@gmail.com>
2017-04-28 12:54:54 +0900

    arrange the training data

--- image_load_test.py
+++ image_load_test.py
@@ -1,15 +1,37 @@
-import tensorflow as tf
-import random
+import os
 import matplotlib.pyplot as plt
+import numpy as np
+
 from PIL import Image
 
-img = Image.open("skive/normal_img/0.png").convert("RGB")
-plt.imshow(img)
-plt.show()
 
-pixels = img.load()
-(width, height) = img.size
+def loadImage(imageFile, labelFile):
+    xImage = Image.open(imageFile).convert("L")
+    yLabel = Image.open(labelFile).convert("L")
+    plt.imshow(xImage, cmap='Greys')
+    plt.show()
+    plt.imshow(yLabel, cmap='Greys')
+    plt.show()
 
-for y in range(height):
-    for x in range(width):
-        print(y, x, pixels[x,y])
+    xPix = np.array(xImage)
+    yPix = np.array(yLabel)
+    print(xPix.shape)
+    print(yPix.shape)
+
+    # data image of shape 300 * 1660 * 1 = 498000
+    #(width, height) = xImage.size
+    #for y in range(height):
+    #    for x in range(width):
+    #        print(y, x, xPix[x,y], yPix[x,y])
+
+    x = np.concatenate(xPix)
+    y = np.concatenate(yPix)
+    xy = np.vstack((x, y))
+    print(xy.shape)
+
+    return xy
+
+
+if __name__ == '__main__':
+    #loadImage("skive/normal_img/0.png")
+    xy = loadImage("skive/defect_img/image_0.png", "skive/defect_img/label_0.png")
